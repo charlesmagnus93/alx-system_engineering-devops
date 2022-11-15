50fa4731583732c4caa614621624404a21503b47
ch@rlemm@gne <agbolossouc@gmail.com>
2022-11-15 00:34:59 +0000

    Well

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""
+Get given user ID TODOS
+"""
+
+if __name__ == "__main__":
+    import requests
+    import sys
+
+    def getUser(id):
+        res = requests.get('https://jsonplaceholder.typicode.com/users/' + id)
+        return res.json()
+
+    def getTodos(userID, taskDone=None):
+        url = 'https://jsonplaceholder.typicode.com/users/' + userID + '/todos'
+
+        if taskDone is not None:
+            if taskDone:
+                url += '?completed=true'
+            else:
+                urm += '?completed=false'
+
+        res = requests.get(url)
+        return res.json()
+
+    
+    userID = sys.argv[1]
+
+    if not userID.isnumeric():
+        sys.exit(0)
+    
+    user = getUser(userID)
+    
+    if not user:
+        sys.exit(0)
+
+    todos = getTodos(userID)
+    taksCompleted = [todo for todo in todos if todo['completed']]
+
+    print('Employee {} is done with tasks({}):'.format(
+        user['name'],
+        len(taksCompleted),
+        len(todos)
+    ))
+
+    for todo in taksCompleted:
+        print('\t' + todo['title'])
+
