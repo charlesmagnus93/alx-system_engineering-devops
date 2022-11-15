a2991e8d3220dcddf8dda4a708d01ae8a93849b9
ch@rlemm@gne <agbolossouc@gmail.com>
2022-11-15 00:49:36 +0000

    good

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -2,49 +2,47 @@
 """
 Get given user ID TODOS
 """
+import requests
+import sys
 
-if __name__ == "__main__":
-	import requests
-	import sys
 
-	def getUser(id):
-		res = requests.get('https://jsonplaceholder.typicode.com/users/' + id)
-		return res.json()
+def getUser(id):
+    r = requests.get('https://jsonplaceholder.typicode.com/users/' + id)
+    return r.json()
 
-	def getTodos(userID, taskDone=None):
-		url = 'https://jsonplaceholder.typicode.com/users/' + userID + '/todos'
 
-		if taskDone is not None:
-			if taskDone:
-				url += '?completed=true'
-			else:
-				url	 += '?completed=false'
+def getTodos(userID, done=None):
+    url = 'https://jsonplaceholder.typicode.com/users/' + userID + '/todos'
 
-		res = requests.get(url)
-		return res.json()
+    if done is not None:
+        if done:
+            url += '?completed=true'
+        else:
+            url += '?completed=false'
+    r = requests.get(url)
+    return r.json()
 
-		if len(sys.argv) == 1:
-		sys.exit(0)
-    
-		userID = sys.argv[1]
 
-		if not userID.isnumeric():
-			sys.exit(0)
-    
-		user = getUser(userID)
-    
-		if not user:
-			sys.exit(0)
+if __name__ == '__main__':
+    if len(sys.argv) == 1:
+        sys.exit(0)
 
-		todos = getTodos(userID)
-			taksCompleted = [todo for todo in todos if todo['completed']]
+    userID = sys.argv[1]
 
-		print('Employee {} is done with tasks({}):'.format(
-			user['name'],
-			len(taksCompleted),
-			len(todos)
-		))
+    if not userID.isnumeric():
+        sys.exit(0)
 
-		for todo in taksCompleted:
-			print('\t' + todo['title'])
+    user = getUser(userID)
+    if not user:
+        sys.exit(0)
 
+    todos = getTodos(userID)
+    taksCompleted = [todo for todo in todos if todo['completed']]
+
+    print('Employee {} is done with tasks({}/{}):'.format(
+        user['name'],
+        len(taksCompleted),
+        len(todos)))
+
+    for todo in taksCompleted:
+        print('\t ' + todo['title'])
