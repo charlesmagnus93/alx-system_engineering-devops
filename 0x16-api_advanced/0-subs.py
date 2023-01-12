5a381506e44040bc7daa33b210272b5928ccc8bf
agbolossouc <agbolossouc@gmail.com>
2023-01-12 17:23:51 +0000

    subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,15 +6,15 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-	"""
-	Request API
-	"""
-	headers = {
-		"User-Agent": "API advanced"
-	}
-	url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-	res = requests.get(url, headers=headers, allow_redirects=False)
-	if res.status_code != 200:
-		return 0
-	result = res.json().get('data').get('subscribers')
-	return result
+    """
+    Request API
+    """
+    headers = {
+      "User-Agent": "API advanced"
+    }
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    res = requests.get(url, headers=headers, allow_redirects=False)
+    if res.status_code != 200:
+        return 0
+    result = res.json().get('data').get('subscribers')
+    return result
