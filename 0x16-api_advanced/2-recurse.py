e0988cf029d0c3f47b2ab04dad6c25ecd90ea104
agbolossouc <agbolossouc@gmail.com>
2023-01-12 17:58:10 +0000

    recurse

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -5,7 +5,7 @@ Recurse for titles
 import requests
 
 
-def recurse(subreddit, hot_list=[]):
+def recurse(subreddit, hot_list=[], next_page=None, count=0):
     """
     Title Liste
     """
@@ -13,14 +13,20 @@ def recurse(subreddit, hot_list=[]):
       "User-Agent": "API advanced"
     }
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-    params = {"limit": 10}
+    params = {"limit": 10, "next_page": next_page, "count": count}
     res = requests.get(
         url, headers=headers, params=params, allow_redirects=False
     )
     if res.status_code != 200:
         return None
     result = res.json().get("data")
+    next_page = res.get("next_page")
+    count += res.get('dist')
     children = result.gt("children")
     for child in children:
         title = child.get("data").get("title")
         hot_list.append(title)
+    if next_page is None:
+        return hot_list
+    return recurse(subreddit, hot_list, next_page, count)
+
